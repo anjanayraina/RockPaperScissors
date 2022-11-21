4005980f630f77e9c42ada45567af649aa9bf4fe
Anjanay Raina <anjanayraina326@gmail.com>
2022-11-21 23:33:45 +0530

    Some minor changes

--- main.py
+++ main.py
@@ -56,12 +56,12 @@ def choicemaker(user , pc):
 
 def initilizestate():
     root.geometry('650x450')
-    name.destroy()
-    f1.destroy()
-    inpname.destroy()
-    sub.destroy()
+    userName.destroy()
+    frame1.destroy()
+    inputtedName.destroy()
+    mainFrame.destroy()
 def creategame():
-    L2 = Label(text=f"{nameinp.get()} Score: {userscore}", bg='#4834DF', fg='#ffffff', borderwidth=5, relief=RAISED,
+    L2 = Label(text=f"{userNameInp.get()} Score: {userscore}", bg='#4834DF', fg='#ffffff', borderwidth=5, relief=RAISED,
                font='Rockwell 13 bold', padx=4, pady=2)
     L2.grid(row=5, column=0, pady=15)
     L3 = Label(text=f"PC Score: {pcscore}", bg='#4834DF', fg='white', borderwidth=5, relief=RAISED,
@@ -77,47 +77,47 @@ def getRandomchoice():
 def playGame(val , pc_opt):
     global userscore
     global pcscore
-    global L1
+    global label1
 
     if choicemaker(val, pc_opt) == 1:  # val means what user chose and pc_opt means what pc chose or opted
-        L1 = Label(text='PC Won', font='lucida 15 bold', bg='black', fg='gold')
-        L1.grid(row=6, column=1, pady=15)
+        label1 = Label(text='PC Won', font='lucida 15 bold', bg='black', fg='gold')
+        label1.grid(row=6, column=1, pady=15)
         pcscore += 1
 
     elif choicemaker(val, pc_opt) == 2:
-        L1 = Label(text=f'{nameinp.get()} Won', font='lucida 15 bold', bg='black', fg='gold')
-        L1.grid(row=6, column=1, pady=15)
+        label1 = Label(text=f'{userNameInp.get()} Won', font='lucida 15 bold', bg='black', fg='gold')
+        label1.grid(row=6, column=1, pady=15)
         userscore += 1
 
     elif choicemaker(val, pc_opt) == 3:
-        L1 = Label(text='PC Won', font='lucida 15 bold', bg='black', fg='gold')
-        L1.grid(row=6, column=1, pady=15)
+        label1 = Label(text='PC Won', font='lucida 15 bold', bg='black', fg='gold')
+        label1.grid(row=6, column=1, pady=15)
         pcscore += 1
 
     elif choicemaker(val, pc_opt) == 4:
-        L1 = Label(text=f'{nameinp.get()} Won', font='lucida 15 bold', bg='black', fg='gold')
-        L1.grid(row=6, column=1, pady=15)
+        label1 = Label(text=f'{userNameInp.get()} Won', font='lucida 15 bold', bg='black', fg='gold')
+        label1.grid(row=6, column=1, pady=15)
         userscore += 1
 
     elif choicemaker(val, pc_opt) == 5:
-        L1 = Label(text='PC Won', font='lucida 15 bold', bg='black', fg='gold')
-        L1.grid(row=6, column=1, pady=15)
+        label1 = Label(text='PC Won', font='lucida 15 bold', bg='black', fg='gold')
+        label1.grid(row=6, column=1, pady=15)
         pcscore += 1
 
     elif choicemaker(val, pc_opt) == 6:
-        L1 = Label(text=f'{nameinp.get()} Won', font='lucida 15 bold', bg='black', fg='gold')
-        L1.grid(row=6, column=1, pady=15)
+        label1 = Label(text=f'{userNameInp.get()} Won', font='lucida 15 bold', bg='black', fg='gold')
+        label1.grid(row=6, column=1, pady=15)
         userscore += 1
 
     elif choicemaker(val, pc_opt) == -1:
-        L1 = Label(text=f"It's A Tie", font='lucida 15 bold', bg='black', fg='gold')
-        L1.grid(row=6, column=1, pady=15)
+        label1 = Label(text=f"It's A Tie", font='lucida 15 bold', bg='black', fg='gold')
+        label1.grid(row=6, column=1, pady=15)
 
 
 # Maingame Function will bring a new window of GUI and will provide a platform to play RPS:
 def maingame():
     global userscore, pcscore
-    global nameinp
+    global userNameInp
     global rock, paper, scissor
     initilizestate()
 
@@ -126,9 +126,9 @@ def maingame():
     # Click Function Main Logic:
     def click(event):
         global userscore, pcscore  # These variable will get or count the scores of user and pc
-        global L1  # We are using L1 Label
+        global label1  # We are using label1 Label
         global pcchose  # we are using old pcchose label
-        L1.grid_forget()  # We are forgetting or removing old label so that new text can come properly
+        label1.grid_forget()  # We are forgetting or removing old label so that new text can come properly
         pcchose.destroy()  # We are deleting or removing old one so that new can come properly
 
         val = event.widget.cget('text')  # This command will take the text of button
@@ -150,7 +150,7 @@ def maingame():
 
     head = Label(text='Rock Paper Scissor Game', font='arial 45 bold', bg='gold', fg='white')
     head.grid(columnspan=2, row=0, ipadx=70, padx=33, pady=10)
-    playerone = Label(text=f'Player 1 : {nameinp.get()}', font='lucida 16')
+    playerone = Label(text=f'Player 1 : {userNameInp.get()}', font='lucida 16')
     playerone.grid(row=1, column=0)
     playertwo = Label(text=f'Player 2 : Computer', font='lucida 16')
     playertwo.grid(row=1, column=1)
@@ -188,11 +188,13 @@ def maingame():
 
 
 
-    # Close
+    # Keep Playing Button
     def keepPlayingButton():
         btnclose = Button(text='Keep Playing', bg='green', font='arial 10 bold')
         btnclose.place(x=350, y=350)
     keepPlayingButton()
+
+    # Close Button
     def closeButton():
         btnclose = Button(text='Stop Playing', command=root.destroy, bg='green', font='arial 10 bold')
         btnclose.place(x=500, y=350)
@@ -219,7 +221,7 @@ paper = Button()
 scissor = Button()
 
 # Defining Label to use it later in the program
-L1 = Label()  # This Label will show the who won pc or user
+label1 = Label()  # This Label will show the who won pc or user
 pcchose = Label()  # This Label will show what pc opted or chose
 
 # Frame for first window of game
@@ -228,27 +230,27 @@ def enteringframe():
     pass
 
 
-f1 = Frame(root)
+frame1 = Frame(root)
 
-img = Image.open('main.webp')
-img = img.resize((900, 400))
-pic = ImageTk.PhotoImage(img)
-Lab = Label(f1, image=pic)
-Lab.pack()
-f1.pack()
+mainFrameImage = Image.open('main.webp')
+mainFrameImage = mainFrameImage.resize((900, 400))
+picture = ImageTk.PhotoImage(mainFrameImage)
+mainLabel = Label(frame1, image=picture)
+mainLabel.pack()
+frame1.pack()
 rockImage = Image.open('rockImage.png')
 
 
 # Create some widgets and placed them above the image that's why used place geometry method
-name = Label(root, text='What do you want to be called in this game', font='arial 15 bold')
-name.place(x=300, y=20)
-nameinp = StringVar()  # This variable will store the name of user
-inpname = Entry(root, textvar=nameinp, font='arial 10')
-inpname.bind('<Return>',
-             entergame)  # We binded Return event with inpname entry widget i.e. if enter key is pressed then entergame function will be called
-inpname.place(x=435, y=60)
+userName = Label(root, text='What do you want to be called in this game', font='arial 15 bold')
+userName.place(x=300, y=20)
+userNameInp = StringVar()  # This variable will store the userName of user
+inputtedName = Entry(root, textvar=userNameInp, font='arial 10')
+inputtedName.bind('<Return>',
+             entergame)  # We binded Return event with inputtedName entry widget i.e. if enter key is pressed then entergame function will be called
+inputtedName.place(x=435, y=60)
 
-sub = Button(root, text="Let's Play", font='lucida 10 bold', bg='black', fg='white', command=maingame)
-sub.place(x=475, y=88)
+mainFrame = Button(root, text="Let's Play", font='lucida 10 bold', bg='black', fg='white', command=maingame)
+mainFrame.place(x=475, y=88)
 
 root.mainloop()
